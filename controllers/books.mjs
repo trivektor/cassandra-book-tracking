51577b53f63935c117d5000c07d7dcb39b7ce1e6
Tri Vuong <tri.vuong@apple.com>
2020-04-15 18:06:36 +0700

    add book

--- controllers/books.mjs
+++ controllers/books.mjs
@@ -1,3 +1,7 @@
+import faker from 'faker';
+import uuid from 'node-uuid';
+import cassandra from 'cassandra-driver';
+
 import client from '../cassandra/client.mjs';
 
 import {BOOKS_TABLE} from '../constants/index.mjs';
@@ -7,4 +11,33 @@ export const getBooks = async (req, res) => {
   const result = await client.execute(query, []);
 
   res.json({books: result.rows});
+};
+
+export const createBook = async (req, res) => {
+  const query = `
+  INSERT INTO ${BOOKS_TABLE}
+    (id, title, author, description, isbn)
+  VALUES
+    (:id, :title, :author, :description, :isbn)
+  `;
+
+  client.execute(
+    query,
+    {
+      id: uuid.v4(),
+      title: faker.lorem.words(4),
+      author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      description: faker.lorem.text(),
+      isbn: faker.random.uuid(),
+    },
+    {prepare: true, consistency: cassandra.types.consistencies.localQuorum},
+    (err, result) => {
+      if (err) {
+        console.log({err});
+        res.status(500).json({message: err.toString()});
+      } else {
+        res.json({result});
+      }
+    }
+  );
 };
--- routes/index.mjs
+++ routes/index.mjs
@@ -1,9 +1,13 @@
 import express from 'express';
 
-import {getBooks} from '../controllers/books.mjs';
+import {
+  getBooks,
+  createBook,
+} from '../controllers/books.mjs';
 
 const router = express.Router();
 
 router.get('/books', getBooks);
+router.post('/books', createBook);
 
 export default router;
